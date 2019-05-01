492129b6a0f6f3236be74cb1666ccf73b4f447ff
fogon@yanex.com <fogon333@yandex.com>
2019-05-01 21:22:37 +0300

    experiments with the bot

--- bot.js
+++ bot.js
@@ -3,9 +3,22 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '747941698:AAGazXigJwIIMHjmRgGHhplU5LXerVyZzZM';
 const bot = new TelegramBot(token, {polling: true});
 
-let	user = 'max';
+	
+/*bot.on('message',function(msg){
+	const user = msg.chat.id;
 
-function function_name() {
-	user + 'как дела?';
-}
-console.log (function_name());
+	bot.sendMessage(user,user);
+	//console.log("test");
+});*/
+console.log(bot);
+
+bot.on('message',function(msg){
+	const chatId = msg.chat.id;
+	const text = msg.text;
+
+	const firstName = msg.chat.first_name;
+	const expression = firstName + ', ' + text;
+	console.log(text);
+	console.log(firstName);
+	bot.sendMessage(chatId,expression);
+});
