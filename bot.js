20e0f8a38a610442207ed9bc4f9fe03b0bc3245e
fogon@yanex.com <fogon333@yandex.com>
2019-05-02 17:49:32 +0300

    bot development

--- bot.js
+++ bot.js
@@ -1,24 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api');
-
 const token = '747941698:AAGazXigJwIIMHjmRgGHhplU5LXerVyZzZM';
 const bot = new TelegramBot(token, {polling: true});
 
-	
-/*bot.on('message',function(msg){
-	const user = msg.chat.id;
-
-	bot.sendMessage(user,user);
-	//console.log("test");
-});*/
 console.log(bot);
+var i = 1;
 
+console.log(i);
 bot.on('message',function(msg){
+	i++;
 	const chatId = msg.chat.id;
 	const text = msg.text;
-
 	const firstName = msg.chat.first_name;
-	const expression = firstName + ', ' + text;
+	//const expression = firstName + ', ' + text;
 	console.log(text);
-	console.log(firstName);
-	bot.sendMessage(chatId,expression);
+	//console.log(firstName);
+
+	bot.sendMessage(chatId,'Это сообщение номер '+i);
 });
