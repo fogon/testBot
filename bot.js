fcd38d15f03a09f63a9208c60d5091d987d14ac9
fogon@yanex.com <fogon333@yandex.com>
2019-05-04 17:18:22 +0300

    bot accounts

--- bot.js
+++ bot.js
@@ -2,7 +2,50 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '747941698:AAGazXigJwIIMHjmRgGHhplU5LXerVyZzZM';
 const bot = new TelegramBot(token, {polling: true});
 
-console.log(bot);
+var i = '';
+
+console.log(i);
+
+bot.on('message', function(msg) {
+
+	var chatId = msg.chat.id;
+
+	console.log(chatId);
+
+	if (chatId === 322724363 || 511825463){
+
+		var text = msg.text;
+
+		 var a = text.search('#LKWAVEкурс');
+
+		 console.log(i);
+
+		if (a >= 0) {
+
+			i += text + ' ';
+
+			console.log(i);
+
+		};
+
+			if (text === '/getResult') {
+
+				//console.log(a);
+
+				bot.sendMessage(chatId, i);
+
+			};		
+		
+	};
+
+});
+
+
+
+
+
+
+/*console.log(bot);
 var i = 1;
 
 console.log(i);
@@ -16,4 +59,4 @@ bot.on('message',function(msg){
 	//console.log(firstName);
 
 	bot.sendMessage(chatId,'Это сообщение номер '+i);
-});
+});*/
